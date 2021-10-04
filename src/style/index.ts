addaa3e7fa600b8f695aebf7c9be2f23626a09fe
Richard <51269862+MeRichard123@users.noreply.github.com>
2021-10-04 10:00:24 +0100

    Add Support Query for Scrollbar Colours

--- src/style/index.ts
+++ src/style/index.ts
@@ -2,60 +2,70 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
     html, body, *::after,*::before{
-    margin: 0;
-    padding: 0;
-    box-sizing:border-box;
-    font-family: "Poppins", sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing:border-box;
+        font-family: "Poppins", sans-serif;
     }
+
     input{
         font-family: "Poppins", sans-serif !important;
         /* line-height: 18px; */
     }
+
     /* JqZSw light mode */
     /* EFeDO dark mode */
     body.dark .ReactModal__Content{
-    background: #242424 !important;
-    color: white !important;
+        background: #242424 !important;
+        color: white !important;
     }
+    
     body.dark .ReactModal__Content .close-btn{
-    background: #808080;
-    border-radius: 50%;
-    margin:auto;
+        background: #808080;
+        border-radius: 50%;
+        margin:auto;
     }
 
     a{
-    text-decoration: none;
-    color: currentcolor;
+        text-decoration: none;
+        color: currentcolor;
     }
 
     .close-btn {
-    position: absolute;
-    top: 15px;
-    right: 15px;
-    cursor: pointer;
-    background: none;
-    border: 0;
-    transition: transform 250ms ease;
+        position: absolute;
+        top: 15px;
+        right: 15px;
+        cursor: pointer;
+        background: none;
+        border: 0;
+        transition: transform 250ms ease;
     }
     .close-btn:hover{
-    transform: scale(1.2)
+        transform: scale(1.2)
     }
 
     .tabs ::-webkit-scrollbar{
-    height: 9px;
+        height: 9px;
     }
 
     .tabs ::-webkit-scrollbar-track {
-    background: #eeeeee;
-    border-radius: 100vw;
-    background-clip: content-box; 
+        background: #eeeeee;
+        border-radius: 100vw;
+        background-clip: content-box; 
     }
 
 
     .tabs ::-webkit-scrollbar-thumb {
-    background: #515151;
-    border-radius: 100vw;
-    height: 10px;
+        background: #515151;
+        border-radius: 100vw;
+        height: 10px;
+    }
+
+    @supports (scrollbar-color: red blue){
+        .tabs{
+            scrollbar-color: #515151 #eeeeee;
+            scrollbar-width: thin;
+        }
     }
 
 `;
